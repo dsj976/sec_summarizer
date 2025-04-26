20516874cb9d39d4c6812a4a4ed462ccd0a1202e
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-04-26 13:34:28 +0100

    Add type hints for model and text parameters in Summarizer class

--- src/sec_summarizer/summarizer/base.py
+++ src/sec_summarizer/summarizer/base.py
@@ -2,11 +2,11 @@ from sec_summarizer.summarizer.huggingface_summarizer import HuggingfaceSummariz
 
 
 class Summarizer:
-    def __init__(self, model):
+    def __init__(self, model: str):
         self.model = model
         self.client = self._load_model(model)
 
-    def _load_model(self, model):
+    def _load_model(self, model: str):
         """
         Load the model based on the provided model name.
         """
@@ -17,7 +17,7 @@ class Summarizer:
         msg = f"Model not supported: {model}."
         raise ValueError(msg)
 
-    def summarize(self, text):
+    def summarize(self, text: str) -> str:
         """
         Summarize the given text using the loaded model.
         """
